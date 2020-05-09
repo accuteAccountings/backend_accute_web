18ed8cade5b9510cbc644623c18f913b8c731f3b
Tushar Bhardwaj <iamtushar324@gmail.com>
2020-05-10 03:06:04 +0530

    google supp
    Signed-off-by: Tushar Bhardwaj <iamtushar324@gmail.com>

---
+++ home/precache-manifest.d8bf12800e9595993d079db08ce3faca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "114b5369d5d0268f796d141f24fef75d",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "0da343f7a32805987585",
+    "url": "/home/static/css/main.91422f3e.chunk.css"
+  },
+  {
+    "revision": "2974d83e10551e1fe5fc",
+    "url": "/home/static/js/2.0f6c67ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/home/static/js/2.0f6c67ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0da343f7a32805987585",
+    "url": "/home/static/js/main.b50bb470.chunk.js"
+  },
+  {
+    "revision": "ee572701b93ae59c4b89",
+    "url": "/home/static/js/runtime-main.7fbc4490.js"
+  },
+  {
+    "revision": "4122b3e75727c3d3a3e4574098bd817c",
+    "url": "/home/static/media/cancel.4122b3e7.svg"
+  },
+  {
+    "revision": "8e70b7587fae61d5a55f3cdc596588fa",
+    "url": "/home/static/media/map.8e70b758.svg"
+  },
+  {
+    "revision": "8654efea3531a1b3773760653989eb99",
+    "url": "/home/static/media/menu.8654efea.svg"
+  },
+  {
+    "revision": "afa9bc31db603b4e16cd169c3040a2bd",
+    "url": "/home/static/media/refresh.afa9bc31.svg"
+  }
+]);
