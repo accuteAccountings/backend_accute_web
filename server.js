f18284936272985b7f245e1a68846a62c6dfea42
Tushar Bhardwaj <iamtushar324@gmail.com>
2020-06-27 00:39:24 +0530

    fixed bugs
    Signed-off-by: Tushar Bhardwaj <iamtushar324@gmail.com>

--- server.js
+++ server.js
@@ -1,36 +1,30 @@
 const exp = require('express');
 const app = exp();
-const { db } = require('./db/db');
+const {db} = require('./db/db');
 const session = require('express-session');
-const { auth } = require('./middleware/auth');
+const {auth} = require('./middleware/auth');
 const upload = require('express-fileupload');
 
 // Middlewares s
 
-app.use(
-	session({
-		secret: process.env.session_sec,
-		resave: true,
-		saveUninitialized: true,
-		cookie: {
-			httpOnly: true
-		}
-	})
-);
+app.use(session({
+  secret : process.env.session_sec,
+  resave : true,
+  saveUninitialized : true,
+  cookie : {httpOnly : true}
+}));
 app.use(exp.json());
-app.use(exp.urlencoded({ extended: true }));
+app.use(exp.urlencoded({extended : true}));
 app.use(upload());
-app.get('/', (req, res) => {
-	res.redirect('/home');
-});
+app.get('/', (req, res) => { res.redirect('/home'); });
 app.use('/main', auth, exp.static('./public/main'));
 app.use('/home', exp.static(`${__dirname}/public/home`));
 app.use('/api', require('./routes/api/index').route);
 
-db.sync({alter:true}).then(() => {
-	app.listen(process.env.port, () => {
-		console.log('Server started at http://0.0.0.0:' + process.env.port);
-	});
+db.sync({alter : true}).then(() => {
+  app.listen(process.env.port, () => {
+    console.log('Server started at http://0.0.0.0:' + process.env.port);
+  });
 });
 
 // temp handlers
