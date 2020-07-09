8155f43753af765706745fd989955654658f7e63
nimit05 <nimitwadhwa722@gmail.com>
2020-07-09 13:48:18 +0530

    printed receipt procedure

---
+++ documents/index.js
@@ -0,0 +1,5 @@
+
+module.exports = ({ name, price1, price2, receiptId }) => {
+    const today = new Date();
+return today
+};
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const exp = require('express');
 const app = exp();
 const { db } = require('./db/db');
+const pdf = require('html-pdf')
 const session = require('express-session');
 const { auth } = require('./middleware/auth');
 const upload = require('express-fileupload');
 
+const pdfTemplate = require('./documents')
 // Middlewares s
 
 app.use(
@@ -25,7 +27,21 @@ app.use('/main', auth, exp.static('./public/main'));
 app.use('/home', exp.static(`${__dirname}/public/home`));
 app.use('/api', require('./routes/api/index').route);
 
-db.sync({ alter: true }).then(() => {
+app.post('/create-pdf' , (req,res) => {
+	pdf.create(pdfTemplate(req.body) , {}).toFile('result.pdf' , (err) => {
+		if(err){
+			res.send(Promise.reject());
+		}
+
+		 res.send(Promise.resolve());
+	})
+})
+
+app.get('/fetch-pdf' , (req,res) => {
+	res.sendFile(`${__dirname}/result.pdf`	)
+})
+
+db.sync().then(() => {
 	app.listen(process.env.port, () => {
 		console.log('Server started at http://0.0.0.0:' + process.env.port);
 	});
