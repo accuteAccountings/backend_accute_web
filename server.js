029b356c0aac49c48154fc37012338191170f109
Tushar Bhardwaj <iamtushar324@gmail.com>
2020-08-14 22:48:44 +0530

    port change
    Signed-off-by: Tushar Bhardwaj <iamtushar324@gmail.com>

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use("/home", exp.static(`${__dirname}/public/home`));
 app.use("/api", require("./routes/api/index").route);
 
 db.sync().then(() => {
-  app.listen(process.env.port, () => {
+  app.listen(5002, () => {
     console.log("Server started at http://0.0.0.0:" + process.env.port);
   });
 });
