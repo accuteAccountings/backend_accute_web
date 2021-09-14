448c2d323391aece4ae4e4f06078189dcf84667f
nishajakhar <ruhaniroy26396@gmail.com>
2021-09-14 17:54:43 +0530

    fix(): Ledger List API Changes

--- utils/vouchers.js
+++ utils/vouchers.js
@@ -1,26 +1,22 @@
-const {Vouch , JoVouch} = require('../db/db')
-const seq = require('sequelize')
+const { Vouch, JoVouch } = require('../db/db');
+const seq = require('sequelize');
 
-async function Vouchers(supplier){
-    const rec = await Vouch.findAll({
-        where: {
-          [seq.Op.and]: [
-            { [seq.Op.or]: [{supplier }, { customer: supplier }] },
-            {IsDeleted : false }
-          ]
-        }
-      });
-  
-      const recJO = await JoVouch.findAll({
-        where: {
-          [seq.Op.and]: [
-            { [seq.Op.or]: [{ credit_acc: supplier }, { debit_acc: supplier }] },
-            {IsDeleted : false }
-          ]
-        }
-      })
-      var arr = rec.concat(recJO);
-      return arr
+async function Vouchers(supplier) {
+  supplier = parseInt(supplier);
+  const rec = await Vouch.findAll({
+    where: {
+      [seq.Op.and]: [{ [seq.Op.or]: [{ supplier_id: supplier }, { customer_id: supplier }] }, { IsDeleted: false }],
+    },
+  });
+  console.log('I am in voucher ledger balaalla', supplier, rec);
+
+  const recJO = await JoVouch.findAll({
+    where: {
+      [seq.Op.and]: [{ [seq.Op.or]: [{ credit_acc_id: supplier }, { debit_acc_id: supplier }] }, { IsDeleted: false }],
+    },
+  });
+  var arr = rec.concat(recJO);
+  return arr;
 }
 
-module.exports = {Vouchers}
+module.exports = { Vouchers };
